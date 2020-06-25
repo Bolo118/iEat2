e45d219a32e3027d1d41d7c0011597476d479558
adithep <adtp2221@gmail.com>
2020-06-24 18:43:55 -0700

    Initial Commit

---
+++ iEat2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iEat2
+//
+//  Created by Adithep on 6/24/20.
+//  Copyright © 2020 Adithep. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
